e5ca525718435f22ecc92eb2c710d321765db6b0
VladimirTarasovets <tarasovets21@gmail.com>
2022-05-01 10:23:40 +0300

    add files

---
+++ script.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# скрипт который выполнит автоматически пункты 3, 4, 5, 6, 7, 8, 13
+
+cd QA_29_free
+mkdir q1 q2 q3
+cd q1
+touch 1.txt 2.txt 3.txt 1.json 2.json
+mkdir w1 w2 w3
+ls -la
+mv 2.txt 3.txt w1/
